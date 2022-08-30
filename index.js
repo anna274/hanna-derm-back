09777df669d1e2c7d8ece1ff5bef73f6fa8acaba
anna274 <anna_rusakovich.01@mail.ru>
2022-08-30 16:51:49 +0300

    feat: add doctors logic

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-const express = require("express");
-const services = require('./data/services.json')
+const express = require('express');
+const cors = require('cors');
+const services = require('./data/services.json');
+const doctors = require('./data/doctors.json');
 const app = express();
 
-app.get("/services", (request, response) => {
-    response.send(services);
+app.use(cors());
+
+app.get('/services', (request, response) => {
+  response.send(services);
 });
 
-app.get("/services/:serviceId", (request, response) => {
-  const service = services.find(({ id }) => {
-    console.log('id', id);
-    console.log('request.params.serviceId', request.params.serviceId);
-    console.log('===',  id === request.params.serviceId)
-    return id === request.params.serviceId
-  })
-  console.log('service', service)
+app.get('/services/:serviceId', (request, response) => {
+  const service = services.find(({ id }) => id === request.params.serviceId);
   response.send(service);
 });
 
+app.get('/doctors', (request, response) => {
+  response.send(doctors);
+});
+
+app.get('/doctors/:doctorId', (request, response) => {
+  const doctor = doctors.find(({ id }) => id === request.params.doctorId);
+  response.send(doctor);
+});
+
 app.listen(8000, () => {
-  console.log('Server started')
+  console.log('Server started');
 });
